31f9d8bb529d9226823560ef72262442d41ae30f
Tyler <tiller1010@gmail.com>
2020-06-09 23:05:53 -0400

    Template updates

--- fatLennysScript.js
+++ fatLennysScript.js
@@ -14,75 +14,93 @@ window.onload=function(){
 	});
 
 	//For carousel
-	const carouselImages=document.querySelectorAll('.carouselImage');
-	const track=document.querySelector('.carouselTrack');
-	const slides=Array.from(track.children);
-	const previousButton=document.querySelector('.carouselButtonLeft');
-	const nextButton=document.querySelector('.carouselButtonRight');
-	const navCircles=document.querySelector('.carouselNav');
-	const dots = Array.from(navCircles.children);
-
-	const slideSize=slides[0].getBoundingClientRect();
-	const slideWidth=slideSize.width;
+	if($('.carousel').length > 0){
+		const carouselImages=document.querySelectorAll('.carouselImage');
+		const track=document.querySelector('.carouselTrack');
+		const slides=Array.from(track.children);
+		const previousButton=document.querySelector('.carouselButtonLeft');
+		const nextButton=document.querySelector('.carouselButtonRight');
+		const navCircles=document.querySelector('.carouselNav');
+		const dots = Array.from(navCircles.children);
 
 
-	//Display all once loaded
-	const displayImages=(image,index)=>{	
-		image.style.display='block';
+		//Display all once loaded
+		const displayImages=(image,index)=>{	
+			image.style.display='block';
+		}
+
+		//Arrangement left to right
+		const setSlidePosition=(slide,index)=>{
+			let slideSize=slides[0].getBoundingClientRect();
+			let slideWidth=slideSize.width;
+			slide.style.left=slideWidth*index+'px';
+		}
+
+		//Display images in a row
+		carouselImages.forEach(displayImages);
+		slides.forEach(setSlidePosition);
+
+		const moveToSlide=(track, currentSlide, targetSlide)=>{
+			track.style.transform='translateX(-'+targetSlide.style.left+')';
+			currentSlide.classList.remove('currentSlide');
+			targetSlide.classList.add('currentSlide');
+		}
+
+		const upDateDots=(currentDot, targetDot)=>{
+			currentDot.classList.remove('currentSlide')
+			targetDot.classList.add('currentSlide')
+		}
+
+		//Indicators to change slide on click
+		navCircles.addEventListener('click',event=>{
+			const targetDot=event.target.closest('button');
+			if(!targetDot) return;
+			const currentSlide=track.querySelector('.currentSlide');
+			const currentDot=navCircles.querySelector('.currentSlide');
+			const targetIndex=dots.findIndex(dot=> dot===targetDot);
+			const targetSlide=slides[targetIndex];
+
+			moveToSlide(track, currentSlide, targetSlide);
+			upDateDots(currentDot, targetDot);
+		})
+
+		nextButton.addEventListener('click', event=>{
+			const currentSlide=track.querySelector('.currentSlide');
+			if(currentSlide.nextElementSibling){
+				var nextSlide=currentSlide.nextElementSibling;
+			} else {
+				var nextSlide = slides[0];
+			}
+
+			moveToSlide(track,currentSlide,nextSlide);
+
+			const currentDot=navCircles.querySelector('.currentSlide');
+			const nextDot=currentDot.nextElementSibling;
+
+			upDateDots(currentDot, nextDot);
+		});
+
+		previousButton.addEventListener('click', event=>{
+			const currentSlide=track.querySelector('.currentSlide');
+			if(currentSlide.previousElementSibling){
+				var previousSlide=currentSlide.previousElementSibling;
+			} else {
+				var previousSlide = slides[slides.length - 1];
+			}
+
+			moveToSlide(track,currentSlide,previousSlide);
+
+			const currentDot=navCircles.querySelector('.currentSlide');
+			const previousDot=currentDot.previousElementSibling;
+
+			upDateDots(currentDot, previousDot);
+		});
+
+		// Fix carousel on resize
+		$(window).on('resize', function(){
+			slides.forEach(setSlidePosition);
+		});
 	}
-
-	//Arrangement left to right
-	const setSlidePosition=(slide,index)=>{
-		slide.style.left=slideWidth*index+'px';
-	}
-
-	carouselImages.forEach(displayImages);
-	slides.forEach(setSlidePosition);
-
-	const moveToSlide=(track, currentSlide, targetSlide)=>{
-		track.style.transform='translateX(-'+targetSlide.style.left+')';
-		currentSlide.classList.remove('currentSlide');
-		targetSlide.classList.add('currentSlide');
-	}
-
-	const upDateDots=(currentDot, targetDot)=>{
-		currentDot.classList.remove('currentSlide')
-		targetDot.classList.add('currentSlide')
-	}
-
-
-	nextButton.addEventListener('click', event=>{
-		const currentSlide=track.querySelector('.currentSlide');
-		const nextSlide=currentSlide.nextElementSibling;
-		const currentDot=navCircles.querySelector('.currentSlide');
-		const nextDot=currentDot.nextElementSibling;
-
-		moveToSlide(track,currentSlide,nextSlide);
-		upDateDots(currentDot, nextDot);
-	});
-
-	previousButton.addEventListener('click', event=>{
-		const currentSlide=track.querySelector('.currentSlide');
-		const previousSlide=currentSlide.previousElementSibling;
-		const currentDot=navCircles.querySelector('.currentSlide');
-		const previousDot=currentDot.previousElementSibling;
-
-		moveToSlide(track,currentSlide,previousSlide);
-		upDateDots(currentDot, previousDot);
-	});
-
-	//Indicators to change slide on click
-	navCircles.addEventListener('click',event=>{
-		const targetDot=event.target.closest('button');
-		if(!targetDot) return;
-		const currentSlide=track.querySelector('.currentSlide');
-		const currentDot=navCircles.querySelector('.currentSlide');
-		const targetIndex=dots.findIndex(dot=> dot===targetDot);
-		const targetSlide=slides[targetIndex];
-
-		moveToSlide(track, currentSlide, targetSlide);
-		upDateDots(currentDot, targetDot);
-	})
 
 
 	//Fade "Ice Cream"
